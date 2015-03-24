eb91d7aea5c81e8ac795b8b9afa22cb847522406
Jacob Ferdinandsen <Jacobferdinandsen@gmail.com>
2015-03-24 09:11:33 +0100

    Startet på game gui

--- src/main/java/com/mycompany/hangman/GameGui.java
+++ src/main/java/com/mycompany/hangman/GameGui.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.hangman;
 
 /**
@@ -10,5 +5,44 @@ package com.mycompany.hangman;
  * @author Jacob
  */
 public class GameGui {
-    
+
+    boolean stillPlaying = true;
+    String answer = "flot";
+    int attempts = 0;
+    int remainingAttempts = 7;
+
+    public void startGame() {
+        System.out.println("Welcome to Hangman");
+        System.out.println("You have " + remainingAttempts + " attempts to solve the word.");
+    }
+
+    public void testForWinner(String guess) {
+        if (guess.equals(answer)) {
+            stillPlaying = false;
+            gameWon();
+        }
+        if (attempts == 7) {
+            stillPlaying = false;
+            newGame();
+        }
+    }
+
+    public void gameWon() {
+        System.out.println("Congratulations");
+        System.out.println("You guessed it!");
+        System.out.println("Using " + attempts + "attempts.");
+    }
+
+    private void newGame() {
+        System.out.println("Want to play a new game?");
+        System.out.println("type 'y'");
+        //read char her
+        String s = "reader her!";
+        if (s == "y") {
+            startGame();
+        } else {
+            System.out.println("Thank you for playing");
+        }
+    }
+
 }
