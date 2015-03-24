237dda7e57742356d0bd3b55e28ae8fc80bc169e
Jacob Ferdinandsen <Jacobferdinandsen@gmail.com>
2015-03-24 08:31:13 +0100

    Junk deleted

--- src/main/java/com/mycompany/hangman/App.java
+++ src/main/java/com/mycompany/hangman/App.java
@@ -1,14 +1,9 @@
 package com.mycompany.hangman;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        //André lugter !
-        System.out.println( "Hello World!" );
+            System.out.println( "Hello World!" );
     }
 }
